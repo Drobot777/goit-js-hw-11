e99c9abe915990f815feff9973315f2a8fd4990f
Drobot777 <121822078+Drobot777@users.noreply.github.com>
2023-06-21 21:14:33 +0300

    Final change yomwork11

--- src/js/api.js
+++ src/js/api.js
@@ -1,85 +1,13 @@
-export {createCards};
-export {findImages};
-export {hidingBtnLoadMore};
 import axios from 'axios';
-import {Notify} from 'notiflix';
-// import simpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.css';
-import {nameImages} from './homwork11';
-import {galleryEl} from './homwork11';
-import {buttonEl} from './homwork11';
-export {page, perPage};
-let page = 0;
-let perPage = 40;
+export {findImages};
 
-async function findImages () {
-  let perPg = 40;
-  perPage = perPg;
-  page += 1;
+async function findImages (name, perPg, pag) {
   const bazUrl = 'https://pixabay.com/api/';
   const keyApi = '?key=37398613-53a7a48ec351839bec483185f';
   const auxiliaryUrl =
     '&image_type=photo&orientation=horizontal&safesearch=true';
   const respons = await axios.get (
-    `${bazUrl}${keyApi}&q=${nameImages}${auxiliaryUrl}&per_page=${perPg}&page=${page}`
+    `${bazUrl}${keyApi}&q=${name}${auxiliaryUrl}&per_page=${perPg}&page=${pag}`
   );
   return respons;
-}
-
-function createCards (arr) {
-  const object = arr.data.hits;
-  const card = object
-    .map (
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return ` <div class="photo-card">
-   
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" width = "300" height="250"/>
-    
-    <div class="info">
-    <p class="info-item">
-    <b>Likes</b>
-    ${likes}
-    </p>
-    <p class="info-item">
-    <b>Views</b>
-    ${views}
-    </p>
-    <p class="info-item">
-    <b>Comments</b>
-    ${comments}
-    </p>
-    <p class="info-item">
-    <b>Downloads</b>
-    ${downloads}
-    </p>
-    </div>
-    </div> `;
-      }
-    )
-    .join ('');
-  return galleryEl.insertAdjacentHTML ('beforeend', card)
-  // new SimpleLightbox ('.gallery a', {
-  //   captionsData: 'alt',
-  //   captionsDelay: 250,
-  //   disableScroll: false,
-  // }).refresh()
-}
-
-function hidingBtnLoadMore (total) {
-  let comparison = page * perPage < total;
-  if (!comparison) {
-    (buttonEl.disabled = true), Notify.failure (
-      "We're sorry, but you've reached the end of search results."
-    );
-  } else {
-    return;
-  }
 }
--- src/js/homwork11.js
+++ src/js/homwork11.js
@@ -1,25 +1,19 @@
 import {Notify} from 'notiflix/build/notiflix-notify-aio';
-import {createCards} from './api';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import {findImages} from './api';
-import {page, perPage} from './api';
-import {hidingBtnLoadMore} from './api';
-export {nameImages};
-export {galleryEl};
-export {buttonEl};
 
 const galleryEl = document.querySelector ('.gallery');
 const fromEl = document.querySelector ('#search-form');
 const buttonEl = document.querySelector ('.load-more');
 const inputEl = document.querySelector ('input');
 let nameImages = '';
-let totalImages = 0;
-
+let page = 0;
+let perPage = 40;
 fromEl.addEventListener ('submit', create);
 buttonEl.addEventListener ('click', loadMore);
 
 function create (e) {
-  perPage = 0;
-  totalImages = 0;
   nameImages = '';
   page = 0;
   galleryEl.innerHTML = '';
@@ -31,7 +25,8 @@ function create (e) {
     );
   }
   nameImages = e.action = inputEl.value;
-  findImages ()
+  page += 1;
+  findImages (nameImages, perPage, page)
     .then (res => {
       if (!res.data.hits.length > 0) {
         Notify.failure (
@@ -40,7 +35,7 @@ function create (e) {
       } else {
         return createCards (res), buttonEl.classList.add (
           'block'
-        ), (totalImages = res.data.totalHits);
+        ), (buttonEl.disabled = false);
       }
     })
     .catch (error => {
@@ -52,14 +47,15 @@ function create (e) {
 
 function loadMore (e) {
   e.preventDefault ();
-  findImages ()
+  page += 1;
+  findImages (nameImages, perPage, page)
     .then (res => {
       if (!res.data.hits.length > 0) {
         Notify.failure (
           'Sorry, there are no images matching your search query. Please try again.'
         );
       } else {
-        return createCards (res), hidingBtnLoadMore (totalImages);
+        return createCards (res), hidingBtnLoadMore (res.data.totalHits);
       }
     })
     .catch (error => {
@@ -67,4 +63,63 @@ function loadMore (e) {
         'Sorry, there are no images matching your search query. Please try again.'
       );
     });
+}
+function createCards (arr) {
+  const object = arr.data.hits;
+  const card = object
+    .map (
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return ` <div class="photo-card">
+   <a class="gallery-link" href="${largeImageURL}">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" width = "300" height="250"/>
+    </a>
+    <div class="info">
+    <p class="info-item">
+    <b>Likes</b>
+    ${likes}
+    </p>
+    <p class="info-item">
+    <b>Views</b>
+    ${views}
+    </p>
+    <p class="info-item">
+    <b>Comments</b>
+    ${comments}
+    </p>
+    <p class="info-item">
+    <b>Downloads</b>
+    ${downloads}
+    </p>
+    </div>
+    </div> `;
+      }
+    )
+    .join ('');
+  return galleryEl.insertAdjacentHTML (
+    'beforeend',
+    card
+  ), new SimpleLightbox ('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+    disableScroll: false,
+  }).refresh ();
+}
+
+function hidingBtnLoadMore (total) {
+  let comparison = page * perPage < total;
+  if (!comparison) {
+    (buttonEl.disabled = true), Notify.failure (
+      "We're sorry, but you've reached the end of search results."
+    );
+  } else {
+    return;
+  }
 }
